3470b3ca0e21816f310936755b2dea8061f21bb6
chonalchendo <110059232+chonalchendo@users.noreply.github.com>
2023-12-13 22:05:33 +0000

    Documentation of LLM code

--- backend/app/llm/model.py
+++ backend/app/llm/model.py
@@ -1,6 +1,7 @@
 import os
 
 from dotenv import load_dotenv
+from typing import Any
 from langchain import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
@@ -32,6 +33,11 @@ avoid repeating yourself. Concisely answer any questions a customer has.
 
 
 def set_custom_prompt():
+    """Sets the prompt template for the chatbot
+
+    Returns:
+        _type_: PromptTemplate
+    """
     prompt = PromptTemplate(
         template=custom_temp, input_variables=["context", "question"]
     )
@@ -39,6 +45,14 @@ def set_custom_prompt():
 
 
 def get_text_chunks(text: str) -> list[str]:
+    """Function that splits the text into chunks
+
+    Args:
+        text (str): text to be split
+
+    Returns:
+        list[str]: list of text chunks
+    """
     text_splitter = CharacterTextSplitter(
         separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len
     )
@@ -46,7 +60,15 @@ def get_text_chunks(text: str) -> list[str]:
     return chunks
 
 
-def get_vectorstore(text: str):
+def get_vectorstore(text: str) -> Any:
+    """Function that creates a vectorstore from the text
+
+    Args:
+        text (str): text to be vectorized
+
+    Returns:
+        Any: vectorstore
+    """
     info = get_text_chunks(text=text)
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_texts(texts=info, embedding=embeddings)
@@ -54,6 +76,11 @@ def get_vectorstore(text: str):
 
 
 def load_llm():
+    """Function that loads the language model
+
+    Returns:
+        _type_: ChatOpenAI
+    """
     llm = ChatOpenAI(
         openai_api_key=OPENAI_API_KEY, model=MODEL, temperature=TEMPERATURE
     )
@@ -61,6 +88,16 @@ def load_llm():
 
 
 def retrival_qa_chain(llm, prompt, db):
+    """Function that creates the retrieval qa chain
+
+    Args:
+        llm (ChatOpenAI): language model
+        prompt (PromptTemplate): prompt template
+        db (FAISS): vectorstore
+
+    Returns:
+        BaseRetrivalQA: retrieval qa chain
+    """
     chain = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
@@ -71,7 +108,15 @@ def retrival_qa_chain(llm, prompt, db):
     return chain
 
 
-def init_ai(text: str):
+def init_ai(text: str) -> RetrievalQA:
+    """Function that initializes the ai
+
+    Args:
+        text (str): text to be vectorized
+
+    Returns:
+        RetrievalQA: retrieval qa chain
+    """
     db = get_vectorstore(text=text)
     llm = load_llm()
     prompt = set_custom_prompt()
@@ -79,7 +124,16 @@ def init_ai(text: str):
     return qa
 
 
-def model(text: str, query: str):
+def model(text: str, query: str) -> dict[str, Any]:
+    """Function that returns the response from the ai
+
+    Args:
+        text (str): text to be vectorized
+        query (str): query to be answered
+
+    Returns:
+        dict[str, Any]: response from the ai
+    """
     result = init_ai(text=text)
     response = result({"query": query})
     return response
